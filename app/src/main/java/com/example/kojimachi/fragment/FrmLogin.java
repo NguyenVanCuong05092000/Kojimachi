8804f74588a19b5428b2fe42375c7bb9a37af88b
NguyenVanCuong05092000 <cuongnguyen05092000@gmail.com>
2021-03-05 16:37:52 +0700

    FrmLogin(In Progress)

--- app/src/main/java/com/example/kojimachi/fragment/FrmLogin.java
+++ app/src/main/java/com/example/kojimachi/fragment/FrmLogin.java
@@ -7,11 +7,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.example.kojimachi.R;
 
 public class FrmLogin extends BaseFragment implements View.OnClickListener {
-
+    private EditText edtLoginID;
+    private EditText edtLoginPass;
     @Override
     protected int getLayoutResId() {
         return R.layout.frm_login;
@@ -29,7 +31,29 @@ public class FrmLogin extends BaseFragment implements View.OnClickListener {
 
     @Override
     protected void loadControlsAndResize(View view) {
+        View imgLogo = view.findViewById(R.id.imgLogo);
+        imgLogo.getLayoutParams().width = actMain.getSizeWithScale(192);
+        imgLogo.getLayoutParams().height = actMain.getSizeWithScale(146);
 
+        View imgLogoText = view.findViewById(R.id.imgLogoText);
+        imgLogoText.getLayoutParams().width = actMain.getSizeWithScale(234);
+        imgLogoText.getLayoutParams().height = actMain.getSizeWithScale(50);
+
+        View clEdtLoginID = view.findViewById(R.id.clEdtLoginID);
+        clEdtLoginID.getLayoutParams().width = actMain.getSizeWithScale(302);
+
+        edtLoginID = view.findViewById(R.id.edtLoginID);
+        edtLoginID.getLayoutParams().height = actMain.getSizeWithScale(46);
+
+        View clEdtLoginPass = view.findViewById(R.id.clEdtLoginPass);
+        clEdtLoginPass.getLayoutParams().width = actMain.getSizeWithScale(302);
+
+        edtLoginPass = view.findViewById(R.id.edtLoginPass);
+        edtLoginPass.getLayoutParams().height = actMain.getSizeWithScale(46);
+
+        View btnLogin = view.findViewById(R.id.btnLogin);
+        btnLogin.getLayoutParams().width = actMain.getSizeWithScale(302);
+        btnLogin.getLayoutParams().height = actMain.getSizeWithScale(46);
     }
 
     @Override
